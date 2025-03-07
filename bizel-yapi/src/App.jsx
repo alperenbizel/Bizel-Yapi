a74e1ffbcbd87372877df0cd5fc0b435bef08178
Alperen Bizel <bizelalperen170@gmail.com>
2025-03-07 23:59:01 +0300

    about section completed

--- bizel-yapi/src/App.jsx
+++ bizel-yapi/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './pages/Header'
+import About from './pages/About'
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
     </div>
   )
 }
---
+++ bizel-yapi/src/pages/About.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef } from "react";
+import "../styles/About.css";
+
+function About() {
+  const aboutRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            aboutRef.current.classList.add("show");
+          } else {
+            aboutRef.current.classList.remove("show");
+          }
+        });
+      },
+      { threshold: 0.3 }
+    );
+
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+
+    return () => {
+      if (aboutRef.current) {
+        observer.unobserve(aboutRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={aboutRef} className="about-container">
+      
+      <div className="about-content">
+      <h1 className="about-title">Hakkımızda</h1>
+        <p className="about-text">
+          Bizel Yapı, 2019 yılında kurulmuş olup, kapı ve pencere sektöründe faaliyet göstermektedir. 
+          <span className="special"> Pencere ve cam aksesuarları, kapı kilitleri, çivi modelleri ve sürme sistemleri</span> gibi geniş bir ürün yelpazesiyle müşterilerine hizmet vermektedir.
+          <br /><br />
+          Firmamız, müşteri memnuniyetini ön planda tutarak, kaliteli ve güvenilir markalarla çalışmaktadır. 
+          İnşaat sektöründe de aktif olarak yer alarak konut, işyeri, altyapı ve üst yapı projeleri gerçekleştirmektedir. 
+          Güvenilirliği ve kaliteyi esas alan hizmet anlayışımızla sektörde fark yaratmaya devam ediyoruz.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
